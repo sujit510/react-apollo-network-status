3e8d344b79e3e7e952af2fdceb15f54ddcaf6be9
Sujit <sujit510@gmail.com>
2020-01-28 16:24:08 +0530

    updated react-apollo-network-status to 3.0.0

--- src/index.js
+++ src/index.js
@@ -1,50 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {ApolloClient} from 'apollo-client';
-import {createHttpLink} from 'apollo-link-http';
-import {ApolloProvider} from 'react-apollo';
-import {ApolloNetworkStatusProvider, useApolloNetworkStatus} from 'react-apollo-network-status';
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import gql from 'graphql-tag'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import {
+  ApolloNetworkStatusProvider,
+  useApolloNetworkStatus
+} from "react-apollo-network-status";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { Query } from "react-apollo";
+import {ApolloProvider} from '@apollo/react-common';
+
+import gql from "graphql-tag";
 
 function GlobalLoadingIndicator() {
   const status = useApolloNetworkStatus();
+
   console.log('status>>%j', status);
-  if (status.numPendingQueries > 0) {
-    return <p>Loading …</p>;
-  } else {
-    return null;
-  }
+
+  return <p>Global status: {JSON.stringify(status)}</p>;
 }
 
-const query = gql`
-  query Continents {
-    continents {
-      code
-      name
-    }
-  }
-`;
+function QueryWithRenderProp() {
+  return (
+    <Query
+      query={gql`
+      query Continents {
+        continents {
+          code
+          name
+        }
+      }
+    `}
+    >
+      {data => {
+          console.log('data>>%j', data);
+          return 'DONE'
+      }}
+    </Query>
+  );
+}
 
-const apolloClient = new ApolloClient({
+const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: createHttpLink({ uri: "https://countries.trevorblades.com/" })
 });
 
-const clickHandler = async () => {
-  const res = await apolloClient.query({
-    query: query
-  });
-  console.log("res>>%j", res);
-};
-
 const element = (
-  <ApolloProvider client={apolloClient}>
+  <ApolloProvider client={client}>
     <ApolloNetworkStatusProvider>
       <GlobalLoadingIndicator />
-      <h3>Hello World!!</h3>
-      <button onClick={clickHandler}>Click Me</button>
+      <QueryWithRenderProp />
     </ApolloNetworkStatusProvider>
   </ApolloProvider>
 );
-ReactDOM.render(element, document.getElementById('root'));
+ReactDOM.render(element, document.getElementById("root"));
